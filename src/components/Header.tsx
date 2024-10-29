f9441e78e5c0d0acae26811c28c57e03b5e20e88
Giacomo Cattani <giacomo.cattani04@gmail.com>
2024-10-29 16:29:39 +0100

    Refactor: Add user profile image to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { redirect } from 'react-router-dom';
 import { Moon, Sun } from 'lucide-react';
+import image from '../assets/user-profile-filled-svgrepo-com.svg';
 
 interface HeaderProps {
     theme: string;
@@ -21,6 +22,12 @@ export const Header: React.FC<HeaderProps> = ({ theme, toggleTheme }) => {
                     <li><div onClick={() => redirect('/cart')} className="cursor-pointer">Cart</div></li>
                     <li><div onClick={() => redirect('/about')} className="cursor-pointer">About</div></li>
                     <li><div onClick={() => redirect('/contact')} className="cursor-pointer">Contact</div></li>
+                    <li>
+                        <div onClick={() => redirect('/profile')} className="cursor-pointer flex items-center space-x-2">
+                            <img src={image} className={`w-7 h-7 mt-1 rounded-full ${theme === 'dark' ? 'filter invert' : ''}`} />
+                            {/* <span>Profile</span> */}
+                        </div>
+                    </li>
                     <li>
                         <button onClick={toggleTheme} className="p-2 rounded-full hover:bg-neutral-200 dark:hover:bg-neutral-700">
                             {theme === 'dark' ? <Sun className="text-yellow-500" size={20} /> : <Moon className="text-neutral-500" size={20} />}
